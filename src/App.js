dfd35fb2b45e7a853f3c0f441f3de16930eb2ca8
lianmalan2 <lianmalan2@gmail.com>
2022-10-06 22:42:48 +0200

    #1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+const Pet = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, { name: 'Nox', animal: 'Cat', breed: 'Street' }),
+    React.createElement(Pet, { name: 'Voldemort', animal: 'Cat', breed: 'Big Fuck-Off' }),
+    React.createElement(Pet, { name: 'Biebie', animal: 'Dog', breed: 'Pekignese' }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
