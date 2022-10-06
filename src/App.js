b571de352758ef4743fcd2975ffa43345ed0be9e
lianmalan2 <lianmalan2@gmail.com>
2022-10-06 22:58:17 +0200

    Initialize git, npm and prettier

--- src/App.js
+++ src/App.js
@@ -10,8 +10,16 @@ const App = () => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, 'Adopt Me!'),
     React.createElement(Pet, { name: 'Nox', animal: 'Cat', breed: 'Street' }),
-    React.createElement(Pet, { name: 'Voldemort', animal: 'Cat', breed: 'Big Fuck-Off' }),
-    React.createElement(Pet, { name: 'Biebie', animal: 'Dog', breed: 'Pekignese' }),
+    React.createElement(Pet, {
+      name: 'Voldemort',
+      animal: 'Cat',
+      breed: 'Big Fuck-Off',
+    }),
+    React.createElement(Pet, {
+      name: 'Biebie',
+      animal: 'Dog',
+      breed: 'Pekignese',
+    }),
   ]);
 };
 
