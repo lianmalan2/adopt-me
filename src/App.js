aa29ad87369b5fed53bd3d0f6935dac3d2495cee
lianmalan2 <lianmalan2@gmail.com>
2022-10-07 07:25:15 +0200

    create Search Params component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
+      <SearchParams />
+      {/*
       <Pet name="Nox" animal="Cat" breed="Street" />
       <Pet name="Voldemort" animal="Cat" breed="Big Fuck-Off" />
       <Pet name="Biebie" animal="Dog" breed="Pekignese" />
+      */}
     </div>
   );
 };
