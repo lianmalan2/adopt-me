35a2451e5071b0c4aacd9b3300765ce94a3614a8
lianmalan2 <lianmalan2@gmail.com>
2022-10-07 07:03:28 +0200

    Refactor App and Pet to jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import { createElement } from 'react';
 import { render } from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-  return createElement('div', {}, [
-    createElement('h1', {}, 'Adopt Me!'),
-    createElement(Pet, { name: 'Nox', animal: 'Cat', breed: 'Street' }),
-    createElement(Pet, {
-      name: 'Voldemort',
-      animal: 'Cat',
-      breed: 'Big Fuck-Off',
-    }),
-    createElement(Pet, {
-      name: 'Biebie',
-      animal: 'Dog',
-      breed: 'Pekignese',
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Nox" animal="Cat" breed="Street" />
+      <Pet name="Voldemort" animal="Cat" breed="Big Fuck-Off" />
+      <Pet name="Biebie" animal="Dog" breed="Pekignese" />
+    </div>
+  );
 };
 
-render(createElement(App), document.getElementById('root'));
+render(<App />, document.getElementById('root'));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,11 @@
-import { createElement } from 'react';
-
 const Pet = (props) => {
-  return createElement('div', {}, [
-    createElement('h1', {}, props.name),
-    createElement('h2', {}, props.animal),
-    createElement('h2', {}, props.breed),
-  ]);
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
 };
 
 export default Pet;
