36b66e97af592393b99a4492ab119c403a9b93b7
lianmalan2 <lianmalan2@gmail.com>
2022-10-07 07:50:38 +0200

    Add breed and implement on Change and on Blur

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,35 @@
 import { useState } from 'react';
 
+const ANIMALS = ['birds', 'cat', 'dog', 'rabbit', 'reptile'];
+
 const SearchParams = () => {
   const [location, setLocation] = useState('');
   const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+  const breeds = [];
+
+  const animalOptions = ANIMALS.map((animal) => (
+    <option key={animal} value={animal}>
+      {animal}
+    </option>
+  ));
+
+  const handleAnimalChange = (e) => {
+    setAnimal(e.target.value);
+    setBreed('');
+  };
+
+  const breedOptions = breeds.map((someBreed) => (
+    <option key={someBreed} value={someBreed}>
+      {someBreed}
+    </option>
+  ));
+
+  const handleBreedChange = (e) => {
+    setBreed(e.target.value);
+  };
+
+  console.warn('STATE', { location, animal, breed });
 
   return (
     <div className="search-params">
@@ -16,14 +43,29 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+
         <label htmlFor="animal">
           Animal
-          <input
+          <select
             id="animal"
             value={animal}
-            placeholder="Animal"
-            onChange={(e) => setAnimal(e.target.value)}
-          />
+            onChange={handleAnimalChange}
+            onBlur={handleAnimalChange}>
+            <option />
+            {animalOptions}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={handleBreedChange}
+            onBlur={handleBreedChange}>
+            <option />
+            {breedOptions}
+          </select>
         </label>
         <button>Submit</button>
       </form>
