9fb295afe2b8cfb191d7ab142e1da434986b3a2a
lianmalan2 <lianmalan2@gmail.com>
2022-10-09 18:12:41 +0200

    Add Strict Mode

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
+import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import SearchParams from './SearchParams';
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-      {/*
-      <Pet name="Nox" animal="Cat" breed="Street" />
-      <Pet name="Voldemort" animal="Cat" breed="Big Fuck-Off" />
-      <Pet name="Biebie" animal="Dog" breed="Pekignese" />
-      */}
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 };
 
