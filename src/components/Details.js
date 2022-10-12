503d4a61b117d3dc5ec01c4be3bc93e9cb9a860a
lianmalan2 <lianmalan2@gmail.com>
2022-10-12 21:55:19 +0200

    Turn Details into a class component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,32 @@
-import { useParams } from 'react-router-dom';
+import { Component } from 'react';
 
-const Details = () => {
-  const { id } = useParams();
-  return <h2>Test header here! [{id}]</h2>;
-};
+class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
