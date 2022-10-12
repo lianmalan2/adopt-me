3056b857de2a143d1b758438f98e35310f2b3016
lianmalan2 <lianmalan2@gmail.com>
2022-10-12 22:06:21 +0200

    Fetch pet details from API

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,24 @@
 import { Component } from 'react';
+import { useParams } from 'react-router-dom';
 
 class Details extends Component {
   constructor(props) {
     super(props);
 
     this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+
+    const json = await res.json();
+
+    this.setState({
+      loading: false,
+      ...json.pets[0],
+    });
   }
 
   render() {
@@ -29,4 +43,9 @@ class Details extends Component {
   }
 }
 
-export default Details;
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+
+export default WrappedDetails;
