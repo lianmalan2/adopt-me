9500f29dcb164b7b2192bcca8ba7e43a5333ffd7
lianmalan2 <lianmalan2@gmail.com>
2022-10-14 09:32:58 +0200

    [Own Rework] Render modal children directly in #modal div

--- src/Modal.js
+++ src/Modal.js
@@ -1,20 +1,10 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 const Modal = ({ children }) => {
-  const elementRef = useRef(null);
-  if (!elementRef.current) {
-    elementRef.current = document.createElement('div');
-  }
+  const elementRef = useRef(document.getElementById('modal'));
 
-  useEffect(() => {
-    const modalRoot = document.getElementById('modal');
-    modalRoot.appendChild(elementRef.current);
-
-    return () => modalRoot.removeChild(elementRef.current);
-  }, []);
-
-  return createPortal(<div>{children}</div>, elementRef.current);
+  return createPortal(children, elementRef.current);
 };
 
 export default Modal;
